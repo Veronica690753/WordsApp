d0849aad3cb2a37c2411d7f90d5cd10cb33cca73
Veronica690753 <veronicaas.ti19@utsjr.edu.mx>
2023-01-06 14:21:39 -0600

    corrección de error en Banner Glossary

--- src/components/Images/BannerGlossary/BannerGlossary.tsx
+++ src/components/Images/BannerGlossary/BannerGlossary.tsx
@@ -13,16 +13,16 @@ interface BannerGlossaryProps {
     selectCategories?: ReactElement,
 }
 
-const BannerGlossary = ({  }: BannerGlossaryProps) => {
+const BannerGlossary = ({ }: BannerGlossaryProps) => {
     const [show, setShow] = useState<boolean>(true);
-    const {setIsOpenModalNewCategory, state } = useContext(TableContext)
-    const {isOpenModalNewCategory} = state
+    const { setIsOpenModalNewCategory, state } = useContext(TableContext)
+    const { isOpenModalNewCategory } = state
     return (
         <>
             <div className={styles.containerHeader}>
                 <div className={styles.containerTop}>
                     <p className={styles.glossary}>Glossary</p>
-                    <ToggleButtonGlossary values={['Words', 'Categories']} onClick= {() => setShow(!show)}/>
+                    <ToggleButtonGlossary values={['Words', 'Categories']} onClick={() => setShow(!show)} />
                     <BasicBtn
                         size="md"
                         fontWeight={700}
@@ -35,66 +35,66 @@ const BannerGlossary = ({  }: BannerGlossaryProps) => {
                 <div className={styles.space}></div>
                 {show ?
                     (
-                    <div className={styles.containerBodyGlossary }>
-                        <div>
-                        <div className={styles.tittle}>
-                            <h2>Term sets simplify searching for definitions</h2>
-                        </div>
-                        <div className={styles.body}>
-                            <p>With the term sets you can find definitions in a simple and easy way. You can associate multiple words within a Category.</p>
-                        </div>
-                        <div className={styles.elementsFooter}>
-                        <InputSearch
-                        size="lg"
-                        type="text"
-                        background="var(--white)"
-                        text="Search Users by name or keyword..."
-                        icon="MagnifyingGlass"
-                        onChange={() => { }}
-                        />
-                        </div>
-                        </div>
+                        <div className={styles.containerBodyGlossary}>
+                            <div>
+                                <div className={styles.tittle}>
+                                    <h2>Term sets simplify searching for definitions</h2>
+                                </div>
+                                <div className={styles.body}>
+                                    <p>With the term sets you can find definitions in a simple and easy way. You can associate multiple words within a Category.</p>
+                                </div>
+                                <div className={styles.elementsFooter}>
+                                    <InputSearch
+                                        size="lg"
+                                        type="text"
+                                        background="var(--white)"
+                                        text="Search Users by name or keyword..."
+                                        icon="MagnifyingGlass"
+                                        onChange={() => { }}
+                                    />
+                                </div>
+                            </div>
 
-                        <img  className={styles.positionImg} src="https://res.cloudinary.com/deej0gb5b/image/upload/v1672684824/Group_45398_puoicf.png" alt="Categories" />
-                    </div>
+                            <img className={styles.positionImg} src="https://res.cloudinary.com/deej0gb5b/image/upload/v1672684824/Group_45398_puoicf.png" alt="Categories" />
+                        </div>
                     )
                     :
-                   (
-                    <div className={styles.containerBodyWords}>
-                        <div>
-                    <div className={styles.tittle}>
-                        <h2>If you are looking for definitions of a term, find it here</h2>
-                    </div>
-                    <div className={styles.body}>
-                        <p>If you need clarification on a word or just want to brush up on your team knowledge, our glossary is here for you.</p>
-                    </div>
-                    <div className={styles.elementsFooter}>
-                        <InputSearch
-                        size="sm"
-                        type="text"
-                        background="var(--white)"
-                        text="Search Users by name or keyword..."
-                        icon="MagnifyingGlass"
-                        onChange={() => { }}
-                        />
-                        <InputCategories />
+                    (
+                        <div className={styles.containerBodyWords}>
+                            <div>
+                                <div className={styles.tittle}>
+                                    <h2>If you are looking for definitions of a term, find it here</h2>
+                                </div>
+                                <div className={styles.body}>
+                                    <p>If you need clarification on a word or just want to brush up on your team knowledge, our glossary is here for you.</p>
+                                </div>
+                                <div className={styles.elementsFooter}>
+                                    <InputSearch
+                                        size="sm"
+                                        type="text"
+                                        background="var(--white)"
+                                        text="Search Users by name or keyword..."
+                                        icon="MagnifyingGlass"
+                                        onChange={() => { }}
+                                    />
+                                    <InputCategories />
+                                </div>
+
+
+                            </div>
+                            <img className={styles.positionImgWords} src="https://res.cloudinary.com/deej0gb5b/image/upload/v1672687254/Group_45280_uxdafp.png" alt="Categories" />
+
                         </div>
-
-
-                </div>
-                <img className={styles.positionImgWords}  src="https://res.cloudinary.com/deej0gb5b/image/upload/v1672687254/Group_45280_uxdafp.png" alt="Categories" />
-
-                </div>
-                   )
+                    )
                 }
 
             </div>
             <Modal
-        callback={(Open) => setIsOpenModalNewCategory(Open)}
-        isOpen={isOpenModalNewCategory}
-      >
-        <ModalNewCategory/>
-      </Modal>
+                callback={(Open) => setIsOpenModalNewCategory(Open)}
+                isOpen={isOpenModalNewCategory}
+            >
+                <ModalNewCategory />
+            </Modal>
 
         </>
     )
--- src/components/Modal/interface/index.tsx
+++ src/components/Modal/interface/index.tsx
@@ -2,7 +2,7 @@ import { ReactNode, Dispatch, SetStateAction } from 'react';
 
 export interface ModalProps {
 	children: ReactNode | JSX.Element[],
-	isOpen: boolean,
-	setIsOpen?: Dispatch<SetStateAction<boolean>>,
+	isOpen: boolean | undefined,
+	setIsOpen?: Dispatch<SetStateAction<boolean>> | undefined,
 	callback?: (isOpen: boolean) => void
 }
