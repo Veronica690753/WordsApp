0b89357e419bd3b8c1aa7aa064023c2450a17da9
Veronica690753 <veronicaas.ti19@utsjr.edu.mx>
2023-01-06 08:32:35 -0600

    arreglo de error usersPage

--- src/components/CardsUserTable/interface/index.tsx
+++ src/components/CardsUserTable/interface/index.tsx
@@ -2,7 +2,7 @@ import { MouseEventHandler } from 'react';
 export interface CardTableProps {
 	name?: string,
 	email?: string,
-	phone?: string,
+	phone?: any,
 	country?: string,
 	hasCheckBox?: boolean,
 	checked?:boolean,
